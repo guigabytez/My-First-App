d668e5d162700ee309671c0510065ca30a43c3d6
guigabytez <ahg0406@gmail.com>
2021-07-06 00:59:42 +0800

    install react-router-dom

--- src/components/navigation/Contents.js
+++ src/components/navigation/Contents.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout } from 'antd'
+import axios from 'axios'
 
 import '../../styles/content.css'
 
@@ -7,17 +8,27 @@ const { Content } = Layout
 
 
 const Contents = () => {
-    return (
-        <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            Content
-        </Content>
-    )
+  useEffect(async () => {
+    // const result = await axios('https://jsonplaceholder.typicode.com').get('/posts')
+
+    // axios('https://jsonplaceholder.typicode.com')
+    //   .get('/posts')
+    //   .then(data => console.log(data))
+    //   .catch(err => console.warn(err))
+
+  }, [])
+
+  return (
+    <Content
+        className="site-layout-background"
+        style={{
+          margin: '24px 16px',
+          padding: 24,
+          minHeight: 280,
+        }}
+      >
+        Content
+    </Content>
+  )
 }
 export default Contents
