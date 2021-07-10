d7751a94c31e2dd2bf5c53e9fef63597989216da
guigabytez <ahg0406@gmail.com>
2021-07-10 13:37:40 +0800

    updates

--- src/menu_pages/Post.js
+++ src/menu_pages/Post.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Card } from 'antd';
 
 const Post = () => {
     return (
-        <div>
-            <h2>Post</h2>
-        </div>
+        <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+        </Card>
     )
 }
 
